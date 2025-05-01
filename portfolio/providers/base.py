c1de5be893119bdbadac9d36d0902199a43a42e4
Cedric McKeever <cedricmckeever@gmail.com>
2025-05-01 16:05:39 +0200

    Update to meet PEP8

--- portfolio/providers/base.py
+++ portfolio/providers/base.py
@@ -28,12 +28,16 @@ def retry(n, wait):
                 try:
                     return func(*args, **kwargs)
                 # Handle execution errors
-                except Exception as e:
+                except Exception:
                     if attempt < n:
-                        print(f'Attempt {attempt}/{n} failed. Retrying in {wait} seconds...')
+                        print(
+                            f'Attempt {attempt}/{n} failed. '
+                            f'Retrying in {wait} seconds...'
+                        )
                         time.sleep(wait)  # Wait before starting next attempt
                     else:
-                        print(f'Attempt {attempt}/{n} failed. Max attempts reached.')
+                        print(f'Attempt {attempt}/{n} failed.'
+                              f'Max attempts reached.')
                         raise
         return wrapper
     return decorator
@@ -43,10 +47,10 @@ class DataProvider():
     """Parent class of data provider sub-classes."""
 
     _ADJ_OPTIONS = ['adjusted', 'unadjusted']
-    
+
     def __init__(self, retry_limit=3, wait=3):
         """Initialise a DataProvider object.
-        
+
         Parameters
         ----------
         retry_limit : int, optional
@@ -57,12 +61,14 @@ class DataProvider():
         self.retry_limit = retry_limit
         self.wait = wait
 
-
-
     @abstractmethod
-    def get_historical(self, tickers: list, sdate:str , edate:str , adj='unadjusted'):
+    def get_historical(self,
+                       tickers: list,
+                       sdate: str,
+                       edate: str,
+                       adj='unadjusted'):
         """Retrieve historical prices from data provider.
-        
+
         Parameters
         ----------
         tickers : list of str
@@ -75,13 +81,14 @@ class DataProvider():
             Adjustment type for the data (default = 'unadjusted').
             Options are:
             - 'unadjusted': Returns unadjusted data (default).
-            - 'adjusted': Returns adjusted data (e.g., for dividends and splits).
+            - 'adjusted': Returns adjusted data (e.g., for dividends and
+            splits).
 
         Returns
         -------
         pandas.DataFrame
-            A dataframe of prices, indexed by dates (rows) and tickers (columns).
+            A dataframe of prices, indexed by dates (rows) and tickers
+            (columns).
         """
-        raise NotImplementedError('Historical data retrieval not supported by this provider.')
-    
-
+        raise NotImplementedError('Historical data retrieval not supported '
+                                  'by this provider.')
--- portfolio/providers/lseg.py
+++ portfolio/providers/lseg.py
@@ -1,46 +1,53 @@
 from .base import DataProvider, retry
 import refinitiv.data as rd
 
+
 class LSEG(DataProvider):
 
-    def get_historical(self, tickers: list, sdate:str, edate:str, adj='unadjusted'):
+    def get_historical(self,
+                       tickers: list,
+                       sdate: str,
+                       edate: str,
+                       adj='unadjusted'):
 
-        @retry(n=self.retry_limit, wait=self.wait)  # Add retry logic  
+        @retry(n=self.retry_limit, wait=self.wait)  # Add retry logic
         def attempt():
 
             print('Attempting LSEG retrieval...')  # Give feedback to user
 
             if adj == 'unadjusted':
                 output = rd.get_history(universe=tickers,
-                                    fields=['TR.CLOSEPRICE(Adjusted=0)'],
-                                    parameters={
+                                        fields=['TR.CLOSEPRICE(Adjusted=0)'],
+                                        parameters={
                                             'SDate': sdate,
                                             'EDate': edate,
                                             'Curn': 'USD',
                                             'Frq': 'D',
                                             },
-                                    )
-            
+                                        )
+
             elif adj == 'adjusted':
                 output = rd.get_history(universe=tickers,
-                                    fields=['TR.CLOSEPRICE'],
-                                    parameters={
+                                        fields=['TR.CLOSEPRICE'],
+                                        parameters={
                                             'SDate': sdate,
                                             'EDate': edate,
                                             'Curn': 'USD',
                                             'Frq': 'D',
                                             },
-                                    )
+                                        )
 
             elif adj not in DataProvider._ADJ_OPTIONS:
-                raise ValueError('Invalid adjustment option.\nPossible:{DataProvider._ADJ_OPTIONS}')
-            
+                raise ValueError('Invalid adjustment option.\n'
+                                 'Possible:{DataProvider._ADJ_OPTIONS}')
+
             else:
-                raise NotImplementedError('The adjustment type is not supported by LSEG')
-            
-            if len(tickers) == 1:  # When fetching for one stock, the col name != the ticker
+                raise NotImplementedError(
+                    'The adjustment type is not supported by LSEG')
+
+            # When fetching for one stock, the col name != the ticker
+            if len(tickers) == 1:
                 output.columns = tickers  # Fix this to ensure compatibility
-            
-            return (output)        
+
+            return (output)
         return attempt()
-    
